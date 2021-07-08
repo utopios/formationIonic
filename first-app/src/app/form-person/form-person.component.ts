f7276a690feec6a92b3ecf59edae8c20c6bb98aa
ihab abadi <utopios@imac-de-ihab.home>
2021-07-08 16:51:59 +0200

    exemple person avec le service

--- first-app/src/app/form-person/form-person.component.ts
+++ first-app/src/app/form-person/form-person.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Person } from '../interfaces/person.interface';
+import { PersonService } from '../services/person.service';
 
 @Component({
   selector: 'app-form-person',
@@ -11,12 +12,13 @@ export class FormPersonComponent implements OnInit {
   lastName:String
   email:String
   @Output() validPerson:EventEmitter<Person> = new EventEmitter()
-  constructor() { }
+  constructor(private personService:PersonService) { }
 
   ngOnInit() {}
 
   confirmForm() {
     const person:Person = {firstName:this.firstName, lastName:this.lastName, email:this.email}
-    this.validPerson.emit(person)
+    //this.validPerson.emit(person)
+    this.personService.addPerson(person)
   }
 }
