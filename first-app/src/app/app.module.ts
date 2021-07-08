8c81ff13945fd7940af2ea727d3464b273e2f4a5
ihab abadi <utopios@imac-de-ihab.home>
2021-07-08 10:19:34 +0200

    notion de module angular

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -7,11 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+//Une fonction qui permet de déclarer une classe comme un module angular, cette fonction prend comme params un objet
+//params => la configuration du module
 @NgModule({
+  //On déclare les composants du modules
   declarations: [AppComponent],
   entryComponents: [],
+  //On importe les sous modules
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  //On indique les services du modules
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  //Le composant principal du module
   bootstrap: [AppComponent],
 })
+//Déclaration du module principal
 export class AppModule {}
