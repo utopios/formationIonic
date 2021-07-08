49fd3c31a00178ddb3069fd8a28939d0dace79e9
ihab abadi <utopios@imac-de-ihab.home>
2021-07-08 12:17:24 +0200

    exemple formulaire person

--- first-app/src/app/person/person.component.ts
+++ first-app/src/app/person/person.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PersonComponent{
   @Input()firstName:String
   @Input()lastName:String
+  @Input() email:String
+  allInformation:String
   age:number
   constructor() { 
     this.age = 0
@@ -15,6 +17,10 @@ export class PersonComponent{
 
   incrementAge() {
     this.age++
+  }
+
+  confirmAllInformation() {
+    this.allInformation = `${this.firstName} ${this.lastName} ${this.age} ${this.email}`
   }
 
 }
