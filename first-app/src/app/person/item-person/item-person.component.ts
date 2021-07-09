69f403fa31e7df5e46735a6d4dd5c7c0ebf18838
ihab abadi <utopios@imac-de-ihab.home>
2021-07-09 11:24:53 +0200

    exemple ngfor avec récupération d'index

--- first-app/src/app/person/item-person/item-person.component.ts
+++ first-app/src/app/person/item-person/item-person.component.ts
@@ -8,6 +8,7 @@ import { Person } from 'src/app/interfaces/person.interface';
   styleUrls: ['./item-person.component.scss'],
 })
 export class ItemPersonComponent implements OnInit {
+  @Input() monIndex:number
   @Input() person:Person
   constructor() { }
 
