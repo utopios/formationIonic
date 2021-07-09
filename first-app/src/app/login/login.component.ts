5bf7b4ea0c48a69483943faf5e8ae0ce8df7e0aa
ihab abadi <utopios@imac-de-ihab.home>
2021-07-09 15:48:09 +0200

    suite reactforms

--- first-app/src/app/login/login.component.ts
+++ first-app/src/app/login/login.component.ts
@@ -17,20 +17,27 @@ export class LoginComponent implements OnInit {
   constructor(private loginService:LoginService, private router:Router, private formBuilder:FormBuilder) {
     //Création d'un formulaire d'une façon réactive
     this.userLogin = this.formBuilder.group({
-      email : new FormControl('', [Validators.required]),
-      password : new FormControl('',[Validators.required])
+      email : new FormControl('', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$")])),
+      password : new FormControl('',Validators.required)
     }) 
    }
 
   ngOnInit() {}
 
   logIn() {
-    if(this.loginService.logIn(this.userLogin.value.email, this.userLogin.value.password)) {
-      this.router.navigate(['/person/form'])
+    
+    if(this.userLogin.status == "VALID") {
+      if(this.loginService.logIn(this.userLogin.value.email, this.userLogin.value.password)) {
+        this.router.navigate(['/person/form'])
+      }
+      else {
+        alert("Erreur de connexion")
+      }
     }
     else {
-      alert("Erreur de connexion")
+      alert("Erreur formulaire")
     }
+    
   }
 
 }
