54f66ffb4232fec8bf7aa2a9e7aa59e793778cda
ihab <ihab@utopios.net>
2021-07-12 14:39:18 +0200

    exemple natif gps et photo

--- second-app/src/app/tab1/tab1.page.ts
+++ second-app/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera } from '@ionic-native/camera/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  constructor(private camera:Camera, private geolocation:Geolocation) {
+    this.geolocation.getCurrentPosition().then(pos=> {
+      this.position = pos.coords.longitude
+    })
+  }
+  position:any
+  takePicture() {
+    this.camera.getPicture({encodingType:this.camera.EncodingType.JPEG}).then((image)=> {
+      alert(image)
+    })
+  }
 }
