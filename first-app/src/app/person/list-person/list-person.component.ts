a6e2bdbfe13d03ce59a458bf723e71a9ff5e74d9
ihab abadi <utopios@imac-de-ihab.home>
2021-07-12 10:41:48 +0200

    suite httpClient

--- first-app/src/app/person/list-person/list-person.component.ts
+++ first-app/src/app/person/list-person/list-person.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { Person } from 'src/app/interfaces/person.interface';
 import { PersonService } from 'src/app/services/person.service';
@@ -10,10 +11,13 @@ import { PersonService } from 'src/app/services/person.service';
 })
 export class ListPersonComponent implements OnInit {
   @Input() listPersons:Array<Person>
-  constructor(private personService:PersonService) { 
+  constructor(private personService:PersonService, private httpClient:HttpClient) { 
     console.log("en cours de chargment....")
-    this.personService.getPersons().then((result:Array<Person>) => {
-      this.listPersons = result
+    // this.personService.getPersons().then((result:Array<Person>) => {
+    //   this.listPersons = result
+    // })
+    this.httpClient.get('http://localhost:3000/persons').subscribe((res:Array<Person>) => {
+      this.listPersons = res
     })
     console.log("l'application continue")
     //this.listPersons = this.personService.getPersons()
